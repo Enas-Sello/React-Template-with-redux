7ca609dd5a99ac215eb39476cf71b032149b1056
Enas Sello <94912513+Enas-Sello@users.noreply.github.com>
2023-03-07 13:55:43 +0200

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {},
+      backgroundColor: {},
+      colors: {},
+      gridTemplateColumns: {
+        view: "max-content 1fr",
+      },
+      gridTemplateRows: {
+        view: "max-content 1fr max-content",
+      },
+    },
   },
   plugins: [],
 }
