5c72d7f59bd3d7491ae48fc53b9c2d5b5f828bea
Enas Sello <94912513+Enas-Sello@users.noreply.github.com>
2023-03-07 13:56:50 +0200

    Update NaveBar.jsx

--- src/components/NaveBar.jsx
+++ src/components/NaveBar.jsx
@@ -1,8 +1,5 @@
 import React from "react"
-// icons
-import { BiSearchAlt } from "react-icons/bi"
-import { IoIosArrowDown } from "react-icons/io"
-import { IoSettingsOutline, IoNotificationsOutline } from "react-icons/io5"
+
 const NaveBar = () => {
   return (
     <nav className="">
