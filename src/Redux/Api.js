697ae3bdd177638a7899b78993210e9440bd6bfd
Enas Sello <94912513+Enas-Sello@users.noreply.github.com>
2023-03-07 13:40:23 +0200

    Update Api.js

--- src/Redux/Api.js
+++ src/Redux/Api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 export const Api = createApi({
   reducerPath: "Api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://www.freetogame.com/api/games",
+    baseUrl: "",
   }),
 
   endpoints: (builder) => ({
